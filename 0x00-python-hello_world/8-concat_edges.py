d8e94fe74bd746ce9d6bf76a7d288ad0a62913ab
BrightDaniel <ivquestservices@gmail.com>
2022-09-19 20:04:25 +0000

    poetry

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+print('''The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.''')
