9b204574886194b9367c386963550b69a9f1285c
Bright Daniel <ivquestservices@gmail.com>
2022-09-20 06:10:46 +0100

    Update 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
