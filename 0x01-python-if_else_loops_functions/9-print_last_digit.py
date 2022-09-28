d28f31bdc7cc8489a8315986580eaf307400861b
Bright Daniel <ivquestservices@gmail.com>
2022-09-28 13:07:47 +0100

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    i = number % 10
-    print('{}'.format(i), end='')
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
