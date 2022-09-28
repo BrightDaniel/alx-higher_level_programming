e081753cde2f7a179d8c67f0a7976d84019a1cb2
Bright Daniel <ivquestservices@gmail.com>
2022-09-28 15:09:46 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     
     b = 5
     
-    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     
-    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     
-    print('{} - {} = {}'.format(a, b, mul(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, mul(a, b)))
     
-    print('{} - {} = {}'.format(a, b, div(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, div(a, b)))
