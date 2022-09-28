2773895318d5672cb59a4a25716aa57b7fd1587a
Bright Daniel <ivquestservices@gmail.com>
2022-09-28 13:47:54 +0100

    Create 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+# def fizzbuzz():
+#     for num in range(1, 101):
+#         if num % 3 == 0 and num % 5 != 0:
+#             print('Fizz', end=' ')
+#         elif num % 5 == 0 and num % 3 != 0:
+#             print('Buzz', end=' ')
+#         elif num % 3 == 0 and num % 5 == 0:
+#             print('FizzBuzz', end=' ')
+#         else:
+#             print(num, end=' ')
