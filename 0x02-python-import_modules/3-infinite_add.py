404b697b5ac5fdbb3ad78d5861f08de4f674958e
Bright Daniel <ivquestservices@gmail.com>
2022-09-29 18:48:41 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-
-    list_of_args = []
-
-    if len(argv) == 1:
-        print("0")
-    else:
-        for i in range(1, len(argv)):
-            list_of_args.append(int(argv[i]))
-
-        print(sum(list_of_args))
+from sys import argv
+add = 0
+for s in argv[1:]:
+    add += int(s)
+print("{:d}".format(add))
