32093f9175005bc89871e9ea5e8761dbf7e79764
Bright Daniel <ivquestservices@gmail.com>
2022-09-30 16:57:54 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+def print_list_integer(my_list=[]):
+  my_list = []
+  print_list_integer(my_list)
