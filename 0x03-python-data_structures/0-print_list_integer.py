db225f14a341048f4318f73691e880883605583e
Bright Daniel <ivquestservices@gmail.com>
2022-09-30 17:07:03 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-
+# !/usr/bin/python3
 def print_list_integer(my_list=[]):
-  for list in my_list:
-    print_list_integer("{}".format(list))
+    for list in my_list:
+        print_list_integer("{}".format(list))
